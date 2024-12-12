4d997339b47c43b9159437a124e543329dc9b751
leishanglin <reasonly8@sina.com>
2024-12-12 12:42:13 +0800

    feat: 初版完成

--- src/extension.ts
+++ src/extension.ts
@@ -33,9 +33,54 @@ export async function activate(context: vscode.ExtensionContext) {
         vscode.window.showInformationMessage("暂存区中没有发现任何文件！");
         return;
       }
+
+      let prompt = "";
       for (const file of stagedFiles) {
         const patch = await repo.diffIndexWithHEAD(file.uri.fsPath);
-        vscode.window.showInformationMessage(patch);
+        prompt += `${patch}\n`;
+        // vscode.window.showInformationMessage(patch);
+      }
+
+      const statusBar = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+      );
+      statusBar.text = "$(sync~spin) Loading..."; // 内置加载动画图标
+
+      // 生产中不要这样写死敏感数据，这里仅作演示，下面的idkey要在演示完成后删除
+      const APP_ID = "bb67b75f5390408a9cc8883055d7bfac";
+      const API_KEY = "sk-03b88fa4b2b44f808251259fa067c7f3";
+      const url = `https://dashscope.aliyuncs.com/api/v1/apps/${APP_ID}/completion`;
+      statusBar.show();
+      try {
+        const res: any = await fetch(url, {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${API_KEY}`,
+          },
+          body: JSON.stringify({
+            input: {
+              prompt,
+            },
+            parameters: {},
+            debug: {},
+          }),
+        }).then((res) => res.json());
+        if (res.output.text) {
+          const re = () => /```\w*([\s\S]*?)```/;
+          let message = "";
+          if (re().test(res.output.text)) {
+            const matched = res.output.text.match(re());
+            message = (matched?.[1] || res.output.text).trim();
+          } else {
+            message = res.output.text.trim();
+          }
+          repo.inputBox.value = message;
+        }
+      } catch {
+        repo.inputBox.value = "罢工了，你手写吧";
+      } finally {
+        statusBar.dispose(); // 完成后移除状态栏
       }
 
       context.subscriptions.push(disposable);
